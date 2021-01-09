81e077831403ab9fd34efdbf2628ea19cea41d81
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-01-09 03:58:21 +0100

    Update install.sh

--- bin/kubelet/ubuntu18/install.sh
+++ bin/kubelet/ubuntu18/install.sh
@@ -9,7 +9,18 @@ set +x && test "$debug" = true && set -x                                ;
 version="1.18.14-00"                                                    ;
 sleep=10                                                                ;
 #########################################################################
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg           ;
+while true                                                              ;
+do                                                                      \
+        systemctl status docker                                         \
+        |                                                               \
+        grep running                                                    \
+        &&                                                              \
+        break                                                           \
+                                                                        ;
+        sleep $sleep                                                    ;
+done                                                                    ;
+#########################################################################
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg           \
 |                                                                       \
 sudo apt-key add -                                                      ;
 echo deb http://apt.kubernetes.io/ kubernetes-xenial main               \
@@ -18,16 +29,5 @@ sudo tee -a /etc/apt/sources.list.d/kubernetes.list                     ;
 sudo apt-get update                                                     ;
 sudo apt-get install -y --allow-downgrades                              \
         kubelet=$version kubeadm=$version kubectl=$version              ;
-sudo systemctl enable --now	kubelet                                 ;
-#########################################################################
-while true                                                              ;
-do                                                                      \
-        systemctl is-enabled kubelet                                    \
-        |                                                               \
-        grep enabled                                                    \
-        &&                                                              \
-        break                                                           \
-                                                                        ;
-        sleep $sleep                                                    ;
-done                                                                    ;
+sudo systemctl enable --now kubelet                                     ;
 #########################################################################
