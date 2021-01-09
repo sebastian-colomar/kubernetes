c749b5640ae70d91e3e28f698338759e29c8db24
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-01-09 03:23:52 +0100

    Update install.sh

--- bin/docker/ubuntu18/install.sh
+++ bin/docker/ubuntu18/install.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+# ./bin/docker/ubuntu18/install.sh
 ################################################################################
 #      Copyright (C) 2020        Sebastian Francisco Colomar Bauza             #
 #      SPDX-License-Identifier:  GPL-2.0-only                                  #
 ################################################################################
-        set -x                                                                 ;
+set -x                                                                         ;
                                                                                #
-        apt-get update                                                         ;
-        apt-get install -y docker.io                                           ;
+apt-get update                                                                 ;
+apt-get install -y docker.io                                                   ;
+systemctl enable docker                                                        ;
+systemctl start docker                                                         ;
+while true                                                                     ;
+  do                                                                           \
+    service docker status | grep running -q && break                           ;
+    sleep 10                                                                   ;
+  done                                                                         ;
 ################################################################################
