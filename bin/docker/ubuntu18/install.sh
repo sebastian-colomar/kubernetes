f0f6a7e869c2c18eeaa1eee8fa72837b5c538ec4
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-01-09 03:57:09 +0100

    Update install.sh

--- bin/docker/ubuntu18/install.sh
+++ bin/docker/ubuntu18/install.sh
@@ -5,14 +5,8 @@
 #      SPDX-License-Identifier:  GPL-2.0-only                                  #
 ################################################################################
 set -x                                                                         ;
-                                                                               #
+################################################################################
 sudo apt-get update                                                            ;
 sudo apt-get install -y docker.io                                              ;
-sudo systemctl enable docker                                                   ;
-sudo systemctl start docker                                                    ;
-while true                                                                     ;
-  do                                                                           \
-    service docker status | grep running -q && break                           ;
-    sleep 10                                                                   ;
-  done                                                                         ;
+sudo systemctl enable --now docker                                             ;
 ################################################################################
