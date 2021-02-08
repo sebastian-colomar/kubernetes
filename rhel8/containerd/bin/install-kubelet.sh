012cb7b4ec7110b2d92d0d211ff6e10ee7770162
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-02-08 09:34:54 +0100

    Update install-kubelet.sh

--- rhel8/containerd/bin/install-kubelet.sh
+++ rhel8/containerd/bin/install-kubelet.sh
@@ -1,41 +1,35 @@
-#!/bin/bash -x
-# ./bin/cluster/ubuntu18/install-kubelet.sh
+#!/bin/sh
 #########################################################################
 #      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
 #      SPDX-License-Identifier:  GPL-2.0-only                           #
 #########################################################################
 set -x                                                                  ;
 #########################################################################
-log=/tmp/install-kubelet.log                                            ;
+baseurl=https://packages.cloud.google.com				;
+engine=containerd							;
+log=/tmp/install-container-kubelet.log                                  ;
+repo=https://download.docker.com/linux/centos/docker-ce.repo		;
+rpm_key=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg	;
 sleep=10                                                                ;
 version="1.18.14-00"                                                    ;
+yum_key=https://packages.cloud.google.com/yum/doc/yum-key.gpg		;
 #########################################################################
-while true                                                              ;
-do                                                                      \
-        systemctl status docker                                         \
-        |                                                               \
-        grep running                                                    \
-        &&                                                              \
-        break                                                           \
-                                                                        ;
-        sleep $sleep                                                    ;
-done                                                                    ;
+sudo tee /etc/yum.repos.d/kubernetes.repo <<EOF
+[kubernetes]
+name=Kubernetes
+baseurl=${baseurl}/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=${yum_key} ${rpm_key}
+exclude=kubelet kubeadm kubectl
+EOF
 #########################################################################
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg           \
-|                                                                       \
-sudo apt-key add -                                                      \
-        2>& 1                                                           \
-|                                                                       \
-tee --append $log                                                       ;
-echo deb http://apt.kubernetes.io/ kubernetes-xenial main               \
-|                                                                       \
-sudo tee -a /etc/apt/sources.list.d/kubernetes.list                     ;
-sudo apt-get update                                                     \
-        2>& 1                                                           \
-|                                                                       \
-tee --append $log                                                       ;
-sudo apt-get install -y --allow-downgrades                              \
-        kubelet=$version kubeadm=$version kubectl=$version              \
+sudo setenforce 0							;
+sudo sed -i /^SELINUX/s/enforcing/permissive/ /etc/selinux/config	;
+#########################################################################
+sudo yum install -y --disableexcludes=kubernetes                        \
+        kubeadm-${version} kubectl-${version} kubelet-${version}        \
         2>& 1                                                           \
 |                                                                       \
 tee --append $log                                                       ;
