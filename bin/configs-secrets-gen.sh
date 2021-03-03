6a1b28f235b8a5d821945f6c6d4679047ca37a02
academiaonline <73463321+academiaonline@users.noreply.github.com>
2021-03-03 03:28:06 +0100

    Update configs-secrets-gen.sh

--- bin/configs-secrets-gen.sh
+++ bin/configs-secrets-gen.sh
@@ -1,21 +1,30 @@
 #!/bin/sh
-################################################################################
-#      Copyright (C) 2020        Sebastian Francisco Colomar Bauza             #
-#      SPDX-License-Identifier:  GPL-2.0-only                                  #
-################################################################################
-        set -x                                                                 ;
-                                                                               #
-        sudo cp -rv run/* /run                                                 ;
-        for config in $( find /run/configs -type f )                           ;
-          do                                                                   \
-            file=$( basename $config )                                         ;
-            kubectl create configmap $file --from-file $config                 ;
-            sudo rm -f $config                                                 ;
-          done                                                                 ;
-        for secret in $( find /run/secrets -type f )                           ;
-          do                                                                   \
-            file=$( basename $secret )                                         ;
-            kubectl create secret generic $file --from-file $secret            ;
-            sudo rm -f $secret                                                 ;
-          done                                                                 ;
-################################################################################
+#################################################################################
+#      Copyright (C) 2020        Sebastian Francisco Colomar Bauza              #
+#      SPDX-License-Identifier:  GPL-2.0-only                                   #
+#################################################################################
+set -x                                                                          ;
+#################################################################################
+sudo cp -rv run/* /run                                                          ;
+for config in $( find /run/configs -type f )                                    ;
+        do                                                                      \
+                file=$( basename ${config} )                                    ;
+                if $( echo ${file} | grep '\.env$' --quiet )                    ;
+                        then                                                    \
+                                kubectl create configmap ${file}                \
+                                        --from-env-file                         \
+                                                ${config}                       ;
+                        else                                                    \
+                                kubectl create configmap ${file}                \
+                                        --from-file                             \
+                                                ${config}                       ;
+                        fi                                                      ;
+                sudo rm -f ${config}                                            ;
+        done                                                                    ;
+for secret in $( find /run/secrets -type f )                                    ;
+        do                                                                      \
+                file=$( basename $secret )                                      ;
+                kubectl create secret generic $file --from-file $secret         ;
+                sudo rm -f $secret                                              ;
+        done                                                                    ;
+#################################################################################
